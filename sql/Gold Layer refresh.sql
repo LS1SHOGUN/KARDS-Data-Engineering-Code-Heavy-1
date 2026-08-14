fec96adb4e9c3e8834a4ab7ca439e87aeaba17bc
LS1SHOGUN <159518700+LS1SHOGUN@users.noreply.github.com>
2026-08-14 18:09:48 +0530

    Add files via upload

---
+++ sql/Gold Layer refresh.sql
@@ -0,0 +1,38 @@
+TRUNCATE TABLE GOLD.spawn_chain;
+INSERT INTO GOLD.spawn_chain
+SELECT k.CardId, k.CardName, k.CardType, k.CardNation, k.CardRarity,
+    k.CardSubType, k.CostToPlay, k.CostToOperate, k.Attack, k.HitPoint,
+    k.Keywords, k.CardEffect, k.IsVeteran, k.VeteranCostToOperate,
+    k.VeteranAttack, k.VeteranHitPoint, k.VeteranKeywords, k.VeteranEffect,
+    k.Status, k.Expansion, k.IsPermanentPool, k.IsSpawnable, k.IsForecastable,
+    s.SpawnCardName, s.SpawnCardType, s.SpawnKeywords, s.SpawnCardCost,
+    s.SpawnCostToOperate, s.SpawnAttack, s.SpawnHitPoint, s.SpawnCardEffect,
+    s.Spawn6KCardName, s.Spawn6KCardEffect, s.Spawn9KCardName, s.Spawn9KCardEffect,
+    s.Spawn12KCardName, s.Spawn12KCardEffect, s.childcardid, cast(GETDATE() as date)
+FROM KardsLakehouse.silver.kards k
+JOIN KardsLakehouse.silver.spawnables s ON k.CardId = s.CardId
+WHERE k.IsSpawnable = 1;
+
+TRUNCATE TABLE GOLD.eligible_for_forecast;
+INSERT INTO GOLD.eligible_for_forecast
+SELECT * FROM KardsLakehouse.silver.kards WHERE IsForecastable = 1;
+
+TRUNCATE TABLE GOLD.veteran_cards;
+INSERT INTO GOLD.veteran_cards
+SELECT * FROM KardsLakehouse.silver.kards WHERE IsVeteran = 1;
+
+TRUNCATE TABLE GOLD.permanent_pool_cards;
+INSERT INTO GOLD.permanent_pool_cards
+SELECT * FROM KardsLakehouse.silver.kards WHERE IsPermanentPool = 1;
+
+TRUNCATE TABLE GOLD.kards;
+INSERT INTO GOLD.kards
+SELECT * FROM KardsLakehouse.silver.kards;
+
+TRUNCATE TABLE GOLD.spawnables;
+INSERT INTO GOLD.spawnables
+SELECT * FROM KardsLakehouse.silver.spawnables;
+
+TRUNCATE TABLE GOLD.forecast;
+INSERT INTO GOLD.forecast
+SELECT * FROM KardsLakehouse.silver.forecast;
---
+++ sql/Gold layer create scripts.sql
@@ -0,0 +1,44 @@
+CREATE schema GOLD;
+
+create table GOLD.spawn_chain
+AS
+SELECT  k.CardId, k.CardName, k.CardType, k.CardNation, k.CardRarity,
+    k.CardSubType, k.CostToPlay, k.CostToOperate, k.Attack, k.HitPoint,
+    k.Keywords, k.CardEffect, k.IsVeteran, k.VeteranCostToOperate,
+    k.VeteranAttack, k.VeteranHitPoint, k.VeteranKeywords, k.VeteranEffect,
+    k.Status, k.Expansion, k.IsPermanentPool, k.IsSpawnable, k.IsForecastable,
+    s.SpawnCardName, s.SpawnCardType, s.SpawnKeywords, s.SpawnCardCost,
+    s.SpawnCostToOperate, s.SpawnAttack, s.SpawnHitPoint, s.SpawnCardEffect,
+    s.Spawn6KCardName, s.Spawn6KCardEffect, s.Spawn9KCardName, s.Spawn9KCardEffect,
+    s.Spawn12KCardName, s.Spawn12KCardEffect, s.childcardid, cast(GETDATE() as date) createddate
+from KardsLakehouse.silver.kards k
+join KardsLakehouse.silver.spawnables s on k.CardId = s.CardId
+where k.IsSpawnable = 1;
+
+
+create table GOLD.eligible_for_forecast
+AS
+select *
+from KardsLakehouse.silver.kards k
+where k.IsForecastable = 1;
+
+create table GOLD.veteran_cards
+AS
+select *
+from KardsLakehouse.silver.kards k
+where k.IsVeteran = 1;
+
+create table GOLD.permanent_pool_cards
+AS
+select *
+from KardsLakehouse.silver.kards k
+where k.IsPermanentPool = 1;
+
+create table GOLD.kards as SELECT * from KardsLakehouse.silver.kards;
+create table GOLD.spawnables as SELECT * from KardsLakehouse.silver.spawnables;
+create table GOLD.forecast as SELECT * from KardsLakehouse.silver.forecast;
+
+
+
+
+
