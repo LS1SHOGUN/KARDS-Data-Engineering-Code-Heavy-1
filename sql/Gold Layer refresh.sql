c2d374c21504a163dadf5705ce1c3dcf4b313c6c
LS1SHOGUN <159518700+LS1SHOGUN@users.noreply.github.com>
2026-08-18 12:47:20 +0530

    Update Gold Layer refresh.sql

--- sql/Gold Layer refresh.sql
+++ sql/Gold Layer refresh.sql
@@ -1,4 +1,30 @@
 --Also Named as Gold_Warehouse_build.sql
+TRUNCATE TABLE GOLD.kards;
+INSERT INTO GOLD.kards
+SELECT * FROM KardsLakehouse.silver.kards;
+
+TRUNCATE TABLE GOLD.spawnables;
+INSERT INTO GOLD.spawnables
+SELECT * FROM KardsLakehouse.silver.spawnables;
+
+TRUNCATE TABLE GOLD.forecast;
+INSERT INTO GOLD.forecast
+SELECT * FROM KardsLakehouse.silver.forecast;
+
+TRUNCATE TABLE GOLD.exile;
+INSERT INTO GOLD.exile
+SELECT * FROM KardsLakehouse.silver.exile;
+
+TRUNCATE TABLE GOLD.synergy_combo_rules;
+INSERT INTO GOLD.synergy_combo_rules
+SELECT * FROM KardsLakehouse.silver.synergy_combo_rules;
+
+TRUNCATE TABLE GOLD.synergy_tag_rules;
+INSERT INTO GOLD.synergy_tag_rules
+SELECT * FROM KardsLakehouse.silver.synergy_tag_rules;
+
+
+
 
 TRUNCATE TABLE GOLD.spawn_chain;
 INSERT INTO GOLD.spawn_chain
@@ -27,14 +53,12 @@ TRUNCATE TABLE GOLD.permanent_pool_cards;
 INSERT INTO GOLD.permanent_pool_cards
 SELECT * FROM KardsLakehouse.silver.kards WHERE IsPermanentPool = 1;
 
-TRUNCATE TABLE GOLD.kards;
-INSERT INTO GOLD.kards
-SELECT * FROM KardsLakehouse.silver.kards;
 
-TRUNCATE TABLE GOLD.spawnables;
-INSERT INTO GOLD.spawnables
-SELECT * FROM KardsLakehouse.silver.spawnables;
-
-TRUNCATE TABLE GOLD.forecast;
-INSERT INTO GOLD.forecast
-SELECT * FROM KardsLakehouse.silver.forecast;
+TRUNCATE TABLE GOLD.exile_data;
+select k.CardId, k.CardName, k.CardType, k.CardNation, k.CardRarity,
+    k.CardSubType, k.CostToPlay, k.CostToOperate, k.Attack, k.HitPoint,
+    k.Keywords, k.CardEffect, k.IsVeteran, k.VeteranCostToOperate,
+    k.VeteranAttack, k.VeteranHitPoint, k.VeteranKeywords, k.VeteranEffect,
+    k.Status, k.Expansion, k.IsPermanentPool, k.IsSpawnable, k.IsForecastable,e.ExileCardId,e.AlsoUsedBy
+from KardsLakehouse.silver.kards k
+join KardsLakehouse.silver.exile e on k.CardId = e.CardId
